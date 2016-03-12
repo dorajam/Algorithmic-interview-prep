bf75e3bb1a66f5005d0f5a5a13bbc0c1589555db
doraki <dorajambor@gmail.com>
2016-03-11 23:21:12 -0500

    updates fib

--- fib_dynamic.py
+++ fib_dynamic.py
@@ -1,9 +1,19 @@
-# linear time
+# Dora Jambor
+# Find the Fibonnaci number - optimized exponential to linear time complexity
+# Dynamic programming prep
 
-if n < 0
-exception
-if n < len(fibs):
-return fibs[n]
-result = fib(n-1) + fib(n-2)
-fibs.append(result)
-return result
+def fib(n):
+	if n < 0:
+		raise ValueError('Invalid input')
+	fibs = [0,1]
+	for i in range(2, n+1):
+		fibs.append(fibs[n-1] + fibs[n-2])
+	print fibs
+	return fibs[n]
+
+# test
+print fib(3)
+print fib(0)
+print fib(1)
+print fib(2)
+print fib(6)
--- fib_tuples.py
+++ fib_tuples.py
@@ -1,13 +1,21 @@
 # Dora Jambor
-# fibonacci
+# Find the Fibonnaci number - optimized exponential to linear time complexity
+# Dynamic programming prep
 
 def fib(n):
 	if n < 0:
-		raise Exception
+		raise ValueError('Invalid value')
 	if n < 2:
 		return n
-	a = 0
-	b = 1
+	a,b = 0,1
+	# n - 1 times
 	for _ in range(1, n):
-		a,b = (b, a + b)
+		a, b = (b, a + b)
 	return b
+
+# test
+print fib(3)
+print fib(0)
+print fib(1)
+print fib(2)
+# print fib(-1)
