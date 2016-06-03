2ded23ebb5ea9e73b70a379404d1f7cf92bef4f2
dorajambor <dorajambor@gmail.com>
2016-06-03 15:09:45 -0400

    graph traverse

---
+++ traverse_graph.py
@@ -0,0 +1,14 @@
+graph = [1,9,3,4,10,6]
+stack = []
+res = []
+
+def DFS(graph, root):
+    stack.push(root)
+    while len(res) < len(graph):
+        u = stack.pop()
+        res.append(u)
+
+        for node in get_adj(u):
+            if node not in path:
+                stack.push(node)
+    return res
